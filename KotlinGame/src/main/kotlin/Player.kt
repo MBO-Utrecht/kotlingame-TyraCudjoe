5736738fc2853ac585e12c8718cd23d4afbe4863
tyra cudjoe <tyra20015@gmail.com>
2021-03-26 10:44:05 +0100

    inital commit

--- KotlinGame/src/main/kotlin/Player.kt
+++ KotlinGame/src/main/kotlin/Player.kt
@@ -1,11 +1,30 @@
 class Player(val name: String, var life: Int = 3, var level: Int = 1, var score: Int = 0) {
+    var weapon: Weapon = Weapon("Fists", 11)
+    val inventory = ArrayList<Loot>()
 
     fun show() {
-        println("""
-            name:  $name
-            life:  $life
-            level: $level
-            score: $score
-            """)
+        if (life < 0) {
+            println("$name is dead!")
+        }else {
+            println("$name is alive!")
+        }
     }
+
+    override fun toString(): String {
+        return """
+            name:       $name
+            life:       $life
+            level:      $level
+            score:      $score
+            weaponName: ${weapon.name}
+            weaponDamg: ${weapon.damageInflicted}
+            """
+    }
+
+    fun showInventory(){
+        println("$name's Invetory:")
+        println(inventory.get(0))
+        println("=================================")
+    }
+
 }
--- KotlinGame/src/main/kotlin/main.kt
+++ KotlinGame/src/main/kotlin/main.kt
@@ -1,5 +1,25 @@
 fun main(args: Array<String>) {
-    val yourName = Player("x", 4, 1, 0)
+    val tyra = Player("Tyra", 4, 1, 0)
 
-    yourName.show()
+    tyra.show()
+
+    println(tyra.weapon.name)
+    println(tyra.weapon.damageInflicted)
+
+    val baksteen = Weapon("Baksteen", 16)
+
+    tyra.weapon = baksteen
+
+    println(tyra.weapon.name)
+    println(tyra.weapon.damageInflicted)
+    println(baksteen.damageInflicted)
+
+    val travon = Player("Travon", 3, 4, 15, baksteen)
+
+    travon.show()
+
+    val redPotion = Loot("Red Potion", LootType.POTION, 12.50)
+
+    travon.inventory.add(redPotion)
+    travon.showInventory().toString()
 }
